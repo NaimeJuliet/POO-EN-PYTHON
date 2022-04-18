637c8dbcb1e624a1a673ffe85f33b87440eba1b1
401 <naime2008@hotmail.com>
2022-04-18 14:22:08 -0500

    NTG-001-Se realiza proyecto con python y programacion orientada a objetos

---
+++ Personaje.py
@@ -0,0 +1,18 @@
+class Personaje:
+    #CONSTRUCTOR
+    #METODO O FUNCION ESPECIAL ENCARGADO DE INICIALIZAR LOS ATRIBUTOS DE UNA CLASE
+    def __init__(self):
+
+        #CREAMOS LOS ATRIBUTOS DE LA CLASE 
+        #ATRIBUTOS=DATOS=VARIABLE
+        self.nombre=None
+        self.tipo=None
+        self.casaeditorial=None
+
+    #DECLARO LOS METODOS O FUNCIONES DE MI CLASE
+    def saludar():
+        print("Hola como estas...")
+
+    
+
+    
---
+++ main.py
@@ -0,0 +1,14 @@
+from Personaje import Personaje
+
+#CREAMOS UNA COPIA DE LA CLASE
+#UTILIZAMOS LA CLASE
+#CREAMOS UNA INSTANCIA DE LA CLASE
+#CREAMOS UN OBJETO DE LA CLASE
+batman= Personaje()
+
+#acceder a los atributos de la clase por medio del objeto
+batman.nombre="J BALVIN"
+print(batman.nombre)
+
+#acceder a los metodos de la clase por medio del objeto
+batman.saludar()
